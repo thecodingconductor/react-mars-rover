8f69579a836e26b8a61f704b3c09f0e841804708
thecodingconductor <tristan.raissherman@gmail.com>
2020-11-28 15:19:17 -0500

    accidentally deleted layout folder...

---
+++ src/components/layout/Spinner.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import spinner from './6.gif';
+
+const Spinner = () => {
+    return (
+        <Fragment>
+            <img src={spinner}></img>
+        </Fragment>
+    )
+}
+
+export default Spinner
