e11a80bb2e9403b597fad2b294b625a03f719802
thecodingconductor <tristan.raissherman@gmail.com>
2020-11-28 16:47:05 -0500

    removed heart when not logged in

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -36,7 +36,8 @@ const Home = () => {
 
                 <div className="main-content-container d-flex flex-column align-items-center justify-content-center">
                     <div className="main-image-container">
-                        <i className="far fa-heart fa-2x add-to-favorites-icon" onClick={onClick} style={{ cursor: "pointer" }}></i>
+                        {user && <i className="far fa-heart fa-2x add-to-favorites-icon" onClick={onClick} style={{ cursor: "pointer" }}></i>}
+
                         <img src={currentImage.img_src} className="main-image" />
                     </div>
                     <Button onClick={generatePhoto} className="generate-photo-button">Generate Photo</Button>
